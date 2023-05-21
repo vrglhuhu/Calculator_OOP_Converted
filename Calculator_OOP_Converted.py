e4b47ca2453bd486ae6d762dcaaef5bcef6ec7b1
vrglhuhu <cheanbvergel@gmail.com>
2023-05-21 14:04:19 +0800

    Finally, i FIXED the probelem that the program can't show the option when the user input invalid.

--- Calculator_OOP_Converted.py
+++ Calculator_OOP_Converted.py
@@ -67,21 +67,15 @@ class Calculator:
 
     # Ask the user if they want to perform another calculation
     def continue_calculation(self):
-        next_calculation = input("\U0001F4CC \033[40m\033[33mDo you want to perform another calculation?\033[0m \033[40m\033[34mYES\033[0m or \033[40m\033[34mNO:\033[0m ")
-        if next_calculation.upper() == "YES":
-           print("\033[31mYEY!\033[0m")
-           return next_calculation
-
-        
-            
-
-
-
-
-
-
-        
-
+        while True:
+            next_calculation = input("\U0001F4CC \033[40m\033[33mDo you want to perform another calculation?\033[0m \033[40m\033[34mYES\033[0m or \033[40m\033[34mNO:\033[0m ")
+            if next_calculation.upper() == "YES":
+                print("\033[31mYEY!\033[0m")
+                return True
+            elif next_calculation.upper() == "NO":
+                return False
+            else:
+                print("\U0001F6A7 \033[31mINVALID INPUT! Please enter either YES or NO.\033[0m\U0001F6A7")
 
 
 
