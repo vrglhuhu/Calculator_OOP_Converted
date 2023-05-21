a88760b97c28c20cfbe5ae8f699dcc4d5d7a1ca1
vrglhuhu <cheanbvergel@gmail.com>
2023-05-21 08:47:06 +0800

    Importing pyfiglet, making class calculator, initializing the operations, and making def for a welcome message.

--- Calculator_OOP_Converted.py
+++ Calculator_OOP_Converted.py
@@ -1,9 +1,32 @@
 # Vergel, Chean Bernard Villanueva
 # Calculator_OOP_Converted
 
+import pyfiglet
+
 # Make class for calculator
+class Calculator:
     # Initialize operations dictionary with operation names as keys and corresponding methods as values 
+    def __init__(self):
+        self.operations = {
+            "a": self.addition,
+            "b": self.subtraction,
+            "c": self.multiplication,
+            "d": self.division
+        }
     # Make a def for the welcome message
+    def print_welcome_message(self):
+        print("=" * 110)
+        print("=" * 110)
+        welcome = pyfiglet.figlet_format("Simple App Calculator".center(55), font="digital")
+        print(welcome)
+        print("=" * 110)
+        print("=" * 110)
+        print("\n\033[40m\033[33mTo select an operation.\033[0m")
+        print("\U0001F6D1 \033[31mPress A: \033[0m Addition")
+        print("\U0001F6D1 \033[32mPress B: \033[0m Subtraction")
+        print("\U0001F6D1 \033[33mPress C: \033[0m Multiplication")
+        print("\U0001F6D1 \033[34mPress D: \033[0m Division\n")
+
     # Get user's choice of operation
     # Perform addition operation and display the result
     # Perform subtraction operation and display the result
