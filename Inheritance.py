981d347eb16d7347be4b2d73a05cd4f189e5a712
vrglhuhu <cheanbvergel@gmail.com>
2023-06-03 13:08:47 +0800

    Making a new method which is fatorial.

--- Inheritance.py
+++ Inheritance.py
@@ -29,3 +29,17 @@ class InheritCalculator(Calculator):
             "E": self.factorial
         }
 
+    def factorial(self):
+        print("\U0001F4C2 \033[40m\033[35mFACTORIAL OPERATION\033[0m \U0001F4C2")
+        try:
+            num = int(input("\U0001F4E2 \033[40m\033[34mEnter the number for factorial calculation:\033[0m "))
+            if num < 0:
+                print("\U0001F6A7 \033[31mINVALID INPUT. Please enter a non-negative integer.\033[0m")
+            else:
+                result = 1
+                for i in range(1, num + 1):
+                    result *= i
+                print("\U0001F7E5 RESULT \U0001F7E5")
+                print(num, "! =", result)
+        except ValueError:
+            print("\U0001F6A7 \033[31mINVALID INPUT. Please enter an integer.\033[0m")
