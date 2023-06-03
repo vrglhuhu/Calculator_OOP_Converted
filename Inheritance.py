de3d337b0e2fb31518b502a28ac67f716416a4b4
vrglhuhu <cheanbvergel@gmail.com>
2023-06-03 12:52:05 +0800

    Importing the code from other py file and making a class to enherit the user interface.

--- Inheritance.py
+++ Inheritance.py
@@ -1,3 +1,9 @@
 # Vergel, Chean Bernard Villanueva
 # Inheritance Calculator
 
+from greetings import Greeting
+from user_interface import User_interface
+from Calculator_OOP_Converted import Calculator
+
+class InheritUserInterface(User_interface):
+    pass
