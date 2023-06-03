14cac11560b38caa6af15ddcef9a76a5de571208
vrglhuhu <cheanbvergel@gmail.com>
2023-06-03 13:30:43 +0800

    Importing the class that needs to perform the calculations.

--- Test_Inheritance_Calculator.py
+++ Test_Inheritance_Calculator.py
@@ -1,2 +1,11 @@
 # Vergel, Chean Bernard Villanueva
 # Inheritance Calculator
+
+from Inheritance import InheritUserInterface
+from Inheritance import InheritCalculator
+from Inheritance import InheritGreeting
+
+greeting = InheritGreeting()
+calculator = InheritCalculator()
+ui = InheritUserInterface()
+greeting.print_welcome_message()
