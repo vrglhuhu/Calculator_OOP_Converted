42afee914af9d0aa25d3936dc3a6acd519f173a7
vrglhuhu <cheanbvergel@gmail.com>
2023-06-03 14:40:13 +0800

    Adding the if else statement in order to work the program and also calling the goodbye message.

--- Test_Inheritance_Calculator.py
+++ Test_Inheritance_Calculator.py
@@ -10,3 +10,23 @@ calculator = InheritCalculator()
 ui = InheritUserInterface()
 greeting.print_welcome_message()
 
+while True:
+    choice = ui.get_user_choice()
+    if choice in calculator.operations:
+        if choice.upper() == "A":
+           calculator.addition()
+        elif choice.upper() == "B":
+           calculator.subtraction()  
+        elif choice.upper() == "C":
+           calculator.multiplication()
+        elif choice.upper() == "D":
+           calculator.division()
+        else:
+           calculator.factorial() 
+        if not ui.continue_calculation():
+            print("\033[32mI hope this program helps and satisfy you.\033[0m")
+            break
+    else:
+        print("\U0001F6A7 \033[31mINVALID INPUT!\033[0m\U0001F6A7")
+
+greeting.print_goodbye_message()
